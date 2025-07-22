d0dc18abb93cdcee41a6f0e276dbdd8fd8ae7826
Code Crafts <codecraftsman12@gmail.com>
2025-07-22 15:44:31 +0600

    changes

--- server/app/firebase.py
+++ server/app/firebase.py
@@ -1,17 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 from dotenv import load_dotenv
-import os, json
+import os
 
 # Load .env variables
 load_dotenv()
 
 # Only initialize once
 if not firebase_admin._apps:
-    cert_data = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    try:
-        cred = credentials.Certificate(json.loads(cert_data))
-    except Exception as e:
-        raise ValueError("Invalid Firebase credentials: ", e)
-
+    cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
     firebase_admin.initialize_app(cred)
