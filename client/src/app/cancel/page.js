006822d7850caa1d67d3a2ab6553b5d402022933
Code Crafts <codecraftsman12@gmail.com>
2025-07-22 17:45:24 +0600

    Add page.js for deployment

--- client/src/app/cancel/page.js
+++ client/src/app/cancel/page.js
@@ -44,7 +44,7 @@ const CancelPage = () => {
             href="/"
             className="inline-block bg-white text-indigo-600 border border-indigo-600 py-2 px-4 rounded-md hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition-colors duration-200 font-medium"
           >
-            Go to Home
+            Go Home
           </Link>
         </div>
       </div>
