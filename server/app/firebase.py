80e5b32aa08f50758d1ce55cd6b0e1bdc9a5f2dd
Code Crafts <codecraftsman12@gmail.com>
2025-07-22 15:40:18 +0600

    changes

--- server/app/firebase.py
+++ server/app/firebase.py
@@ -1,12 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 from dotenv import load_dotenv
-import os
+import os, json
 
 # Load .env variables
 load_dotenv()
 
 # Only initialize once
 if not firebase_admin._apps:
-    cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+    cert_data = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    try:
+        cred = credentials.Certificate(json.loads(cert_data))
+    except Exception as e:
+        raise ValueError("Invalid Firebase credentials: ", e)
+
     firebase_admin.initialize_app(cred)
--- server/main.py
+++ server/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all origins in development
+    allow_origins=["http://localhost:3000"],  # allow all origins in development
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
